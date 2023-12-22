b63080cb6a6ceb4e3158b03371c7f2d464b6c254
armantastan135@gmail.com <armantastan@MacBook-Air-Arman.local>
2023-12-21 17:08:18 -0800

    service registry

--- employee-service/src/main/java/com/arman/employeeservice/service/ApiClient.java
+++ employee-service/src/main/java/com/arman/employeeservice/service/ApiClient.java
@@ -2,11 +2,11 @@ package com.arman.employeeservice.service;
 
 import com.arman.employeeservice.dto.DepartmentDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
+//@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
 public interface ApiClient {
     @GetMapping("api/departments/{departmentCode}")
     DepartmentDto getDepartmentByCode(@PathVariable String departmentCode);
